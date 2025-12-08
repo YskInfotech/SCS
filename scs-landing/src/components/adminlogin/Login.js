cfc7566178874e7ec5c8b662f007230e06d696bf
Revanthsvv <revanthkumar@svvinfotech.in>
2025-12-08 17:12:56 +0530

    Add login slide

---
+++ scs-landing/src/components/adminlogin/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Login (){
+    return(
+        <>
+        <div className="main-cont">
+            <div className="">
+
+            </div>
+        </div>
+        </>
+    )
+}
+export default Login;
